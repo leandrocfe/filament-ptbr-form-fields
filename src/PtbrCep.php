f2c6e8f7e096cf367d7a4c8efb1933101569402a
Rômulo Ramos <rmsramos@gmail.com>
2023-08-14 17:06:53 -0300

    refactoring PtbrCep and add eventFocus

--- src/PtbrCep.php
+++ src/PtbrCep.php
@@ -3,7 +3,8 @@
 namespace Leandrocfe\FilamentPtbrFormFields;
 
 use Filament\Forms\Components\Actions\Action;
-use Filament\Forms\Components\{Component, TextInput};
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
 use Filament\Forms\Set;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
@@ -12,16 +13,9 @@ use Livewire\Component as Livewire;
 
 class PtbrCep extends TextInput
 {
-    /**
-     * Via CEP integration
-     *
-     * @param  string  $mode
-     * @param  string  $errorMessage
-     * @param  array  $setFields
-     */
-    public function viaCep($mode = 'suffix', $errorMessage = 'CEP inválido.', $setFields = []): static
+    public function viaCep(string $mode = 'suffix', string $errorMessage = 'CEP inválido.', string $eventFocus = '', array $setFields = []): static
     {
-        $viaCepRequest = function ($state, Livewire $livewire, Set $set, Component $component, string $errorMessage, array $setFields) {
+        $viaCepRequest = function ($state, $livewire, $set, $component, $errorMessage, $eventFocus, array $setFields) {
 
             $livewire->validateOnly($component->getKey());
 
@@ -36,32 +30,39 @@ class PtbrCep extends TextInput
                     $component->getKey() => $errorMessage,
                 ]);
             }
+
+            if ($eventFocus) {
+                $livewire->dispatch($eventFocus);
+            }
+
         };
 
         $this
             ->minLength(9)
-            ->extraAlpineAttributes(['x-mask' => '99999-999'])
-            ->afterStateUpdated(function ($state, $livewire, $set, $component) use ($errorMessage, $setFields, $viaCepRequest) {
-                $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $setFields);
+            ->mask('99999-999')
+            ->afterStateUpdated(function ($state, Livewire $livewire, Set $set, Component $component) use ($errorMessage, $setFields, $eventFocus, $viaCepRequest) {
+                $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $eventFocus, $setFields);
             })
-            ->suffixAction(function ($state, $livewire, $set, $component) use ($mode, $errorMessage, $setFields, $viaCepRequest) {
+            ->suffixAction(function () use ($mode, $errorMessage, $eventFocus, $setFields, $viaCepRequest) {
                 if ($mode === 'suffix') {
                     return Action::make('search-action')
                         ->label('Buscar CEP')
-                        ->icon('heroicon-o-search')
-                        ->action(function () use ($state, $livewire, $set, $component, $errorMessage, $setFields, $viaCepRequest) {
-                            $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $setFields);
-                        });
+                        ->icon('heroicon-o-magnifying-glass')
+                        ->action(function ($state, Livewire $livewire, Set $set, Component $component) use ($errorMessage, $eventFocus, $setFields, $viaCepRequest) {
+                            $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $eventFocus, $setFields);
+                        })
+                        ->cancelParentActions();
                 }
             })
-            ->prefixAction(function ($state, $livewire, $set, $component) use ($mode, $errorMessage, $setFields, $viaCepRequest) {
+            ->prefixAction(function () use ($mode, $errorMessage, $eventFocus, $setFields, $viaCepRequest) {
                 if ($mode === 'prefix') {
                     return Action::make('search-action')
                         ->label('Buscar CEP')
-                        ->icon('heroicon-o-search')
-                        ->action(function () use ($state, $livewire, $set, $component, $errorMessage, $setFields, $viaCepRequest) {
-                            $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $setFields);
-                        });
+                        ->icon('heroicon-o-magnifying-glass')
+                        ->action(function ($state, Livewire $livewire, Set $set, Component $component) use ($errorMessage, $eventFocus, $setFields, $viaCepRequest) {
+                            $viaCepRequest($state, $livewire, $set, $component, $errorMessage, $eventFocus, $setFields);
+                        })
+                        ->cancelParentActions();
                 }
             });
 
